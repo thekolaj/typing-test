dba01dfef5aeece748374405854a180747259812
KolaJ <thekolaj@gmail.com>
2023-11-02 13:20:45 +0200

    check input

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,7 @@
+// Settings:
+const file = 'texts.json'
+const maxTime = 60;
+
 // Elements:
 const inputField = document.querySelector('.input-field')
 const textDisplay = document.querySelector('.text-display')
@@ -6,18 +10,45 @@ const textDisplay = document.querySelector('.text-display')
 document.addEventListener('keydown', event => {
   inputField.focus()
   if (event.key === 'Escape') console.log(inputField)
-  if (event.key === 'Enter') console.log(textDisplay)
+  if (event.key === 'Enter') startNewTest()
 })
+inputField.addEventListener('input', checkUserInput)
 
+function checkUserInput() {
+  const fullText = textDisplay.querySelectorAll('span')
+  const userInput = inputField.value
+  for (let i = 0; i < fullText.length; i++) {
+    const userCharacter = userInput.charAt(i)
+    if (!userCharacter) {
+      fullText[i].className = ''
+    } else if (userCharacter === fullText[i].innerText) {
+      fullText[i].className = 'correct'
+    } else {
+      fullText[i].className = 'incorrect'
+    }
+  }
+  fullText[userInput.length].className = 'active'
+}
 
-fetch('texts.json')
-  .then(res => res.json())
-  .then(data => {
-    const text = data[Math.floor(Math.random() * data.length)]
-    const textWithHtml = text.split('').reduce((fullText, currentCharacter) => {
-      return fullText + `<span>${currentCharacter}</span>`
-    }, '')
+/**
+ * Get random text from file
+ * @param {string} file Path to JSON file with array of texts.
+ * @returns {string}
+ */
+async function getRandomText(file) {
+   return fetch(file)
+    .then(res => res.json())
+    .then(data => data[Math.floor(Math.random() * data.length)])
+    .catch(() => 'Error! Could not open file: ' + file)
+}
 
+async function startNewTest() {
+  const text = await getRandomText(file)
+  textDisplay.innerHTML = text.split('').reduce((fullText, currentCharacter) => {
+    return fullText + `<span>${currentCharacter}</span>`
+  }, '')
+  textDisplay.querySelector('span').classList.add('active')
+  inputField.value = ''
+}
 
-    textDisplay.innerHTML = textWithHtml
-  })
+startNewTest()
